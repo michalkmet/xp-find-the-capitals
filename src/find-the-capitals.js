032f9b1b75985e110ebe752553ac7ff151234ec9
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-21 16:02:53 +0200

    experimental refactor works

--- src/find-the-capitals.js
+++ src/find-the-capitals.js
@@ -1,13 +1,6 @@
-function findTheCapitals(string) {
-  if (typeof string != 'string') throw new Error('Invalid input');
-
-  // let result = [];
-  // for (let i = 0; i < string.length; i++) {
-  //   if (string[i] !== string[i].toLowerCase()) result.push(i);
-  // }
-  // return result;
-
-  return [...string].map((char, index) => (char !== char.toLowerCase() ? index : -1)).filter((i) => i > -1);
+function findTheCapitals(s) {
+  if (typeof s != 'string') throw new Error('Invalid input');
+  return [...s].map((c, i) => (c !== c.toLowerCase() ? i : -1)).filter((i) => i > -1);
 }
 
 module.exports = findTheCapitals;
